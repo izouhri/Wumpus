2fc4812dbb2488e1089e611864210d94955d859d
zouhri <zouhrimane@gmail.com>
2018-05-06 14:10:23 +0200

    State

--- src/State.java
+++ src/State.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 
 public class State {
     private Coordonnees aventurier;
-    private Coordonnees wompus;
+    private Coordonnees wumpus;
     private ArrayList<Coordonnees> trous;
     private Coordonnees or;
 
-    public State(Coordonnees aventurier, Coordonnees wompus, Coordonnees trous1, Coordonnees trou2, Coordonnees or) {
+    public State(Coordonnees aventurier, Coordonnees wumpus, Coordonnees trous1, Coordonnees trou2, Coordonnees or) {
         this.trous = new ArrayList<Coordonnees>();
         this.trous.add(trous1);
         this.trous.add(trou2);
-        this.wompus = wompus;
+        this.wumpus = wumpus;
         this.or = or;
         this.aventurier = aventurier;
     }
@@ -19,7 +19,7 @@ public class State {
         this.trous = new ArrayList<Coordonnees>();
         this.trous.add(new Coordonnees(2,2));
         this.trous.add(new Coordonnees(3,4));
-        this.wompus = new Coordonnees(1,3);
+        this.wumpus = new Coordonnees(1,3);
         this.or = new Coordonnees(3,3);
         this.aventurier = new Coordonnees(0,0);
     }
@@ -36,8 +36,8 @@ public class State {
         return or;
     }
 
-    public Coordonnees getWompus() {
-        return wompus;
+    public Coordonnees getwumpus() {
+        return wumpus;
     }
 
     public void setAventurier(Coordonnees aventurier) {
@@ -52,7 +52,7 @@ public class State {
         this.trous = trous;
     }
 
-    public void setWompus(Coordonnees wompus) {
-        this.wompus = wompus;
+    public void setwumpus(Coordonnees wumpus) {
+        this.wumpus = wumpus;
     }
 }
