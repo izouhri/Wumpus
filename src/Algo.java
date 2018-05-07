4f44d86c3d9094043425aa312f2fd38d94d1b0b7
Nicolas Paris <>
2018-05-07 22:45:22 +0200

    Maj

--- src/Algo.java
+++ src/Algo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Algo {
 
     public static void main(String[] args) {
@@ -14,6 +16,27 @@ public class Algo {
     public static void solution(Problem p, State s) {
     	
     }
+
+    //regles fonction qui prend etat en pararet et qui te dit la'action a efectuer
+
+    public Action nextMove (State s, Problem p) {
+
+        if (s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERHAUT).getAventurier().getPosition())){
+            return Action.ALLERHAUT;
+        }
+        if (s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERBAS).getAventurier().getPosition())){
+            return Action.ALLERBAS;
+        }
+        if (s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERDROITE).getAventurier().getPosition())){
+            return Action.ALLERDROITE;
+        }
+        if (s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERGAUCHE).getAventurier().getPosition())){
+            return Action.ALLERGAUCHE;
+        }
+        else return Action.ALLERGAUCHE;
+    }
+
+
     
     private static Action agentModel(Observation o, Agent a) {
     	a.setObservation(o);
