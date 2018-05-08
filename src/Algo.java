749173d10d940fa8535a5dbeb972440ff9f9abd8
Nicolas Paris <>
2018-05-08 14:09:30 +0200

    Algo

--- src/Algo.java
+++ src/Algo.java
@@ -18,22 +18,36 @@ public class Algo {
     public static void solution(Problem p, State s) {
     	
     }
-    
+
 
     public Action nextMove(State s, Problem p) {
         Queue<Action> actions = new PriorityQueue<Action>();
-        if (s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERHAUT).getAventurier().getPosition())){
-            actions.add(Action.ALLERHAUT);
+        if (!s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERHAUT).getAventurier().getPosition())){
+            if (s.getAventurier().getObservation((p.transition(s,Action.ALLERHAUT).getAventurier().getPosition())) == null){
+                actions.add(Action.ALLERHAUT); //mettre en priorité
+            }
+            // else Mettre allzeHaut pas priorité
         }
-        if (s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERBAS).getAventurier().getPosition())){
-            actions.add(Action.ALLERBAS);
+        if (!s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERBAS).getAventurier().getPosition())){
+            if (s.getAventurier().getObservation((p.transition(s,Action.ALLERBAS).getAventurier().getPosition())) == null){
+                actions.add(Action.ALLERBAS);
+            }
+            // else pas en priorité actions.add(Action.ALLERBAS);
         }
-        if (s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERDROITE).getAventurier().getPosition())){
-            actions.add(Action.ALLERDROITE);
+        if (!s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERDROITE).getAventurier().getPosition())){
+            if (s.getAventurier().getObservation((p.transition(s,Action.ALLERDROITE).getAventurier().getPosition())) == null){
+                actions.add(Action.ALLERDROITE);
+            }
+            //else pas priorité actions.add(Action.ALLERDROITE);
         }
-        if (s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERGAUCHE).getAventurier().getPosition())){
-            actions.add(Action.ALLERGAUCHE);
+        if (!s.getAventurier().getWhereIsWumpus().contains(p.transition(s,Action.ALLERGAUCHE).getAventurier().getPosition())){
+            if (s.getAventurier().getObservation((p.transition(s,Action.ALLERGAUCHE).getAventurier().getPosition())) == null){
+                actions.add(Action.ALLERGAUCHE);
+            }
+            // pas en priorité actions.add(Action.ALLERGAUCHE);
         }
+        // Si aucune priorité
+
     }
 
 
